3ff47b87674567a25bcc2f2fb16bab9f1627dfe5
Lab User <LabUser@labs.salina.k-state.edu>
2019-09-12 10:51:40 -0500

    added cool route to users

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/cool', function(req, res, next) {
+  res.send('sooooo coooool');
+});
+
+
 module.exports = router;
