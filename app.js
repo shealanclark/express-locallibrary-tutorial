56e44b30380a8d0bb348123e5a81612d60dbcb18
Lab User <LabUser@labs.salina.k-state.edu>
2019-10-01 10:49:38 -0500

    dob and dod for author

--- app.js
+++ app.js
@@ -10,6 +10,13 @@ var catalogRouter = require('./routes/catalog');
 
 var app = express();
 
+//Set up mongoose connection
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb+srv://dbUser:RiotCows77@cluster0-lljhr.gcp.mongodb.net/locallibrary?retryWrites=true&w=majority';
+mongoose.connect(mongoDB, { useNewUrlParser: true });
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
